ec543feb404eddb80a5971b25f627d12c08c7d87
Temur Coder <temurnarzullayev810@gmail.com>
2024-11-20 18:03:27 +0300

    svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 // / @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src//*.{html,js}"],
+  content: ["./public//*.{html,js}"],
   theme: {
     extend: {
       backgroundColor: {
         "body-bg": "#ECECEC",
       },
+      
     },
   },
   plugins: [],
-};
+}
